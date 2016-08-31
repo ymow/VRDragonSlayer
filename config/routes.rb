908804beae0d277aadc25820784bd9aec4df4034
Ymow Wu <kenshin0812@hotmail.com>
2016-08-31 08:44:43 +0800

    rount.rb root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-   root :to => "static_page#Home" 
-     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+  get  'static_pages/home'
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
